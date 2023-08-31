d074a1d41f201c162649c6591e1108462f297d7a
idkmynamereally <97702836+idkmynamereally@users.noreply.github.com>
2023-08-31 19:41:03 +0530

    Create myInput.h

---
+++ myInputLib/myInput.h
@@ -0,0 +1,12 @@
+#ifndef MYINPUT_H
+#define MYINPUT_H
+#include <string>
+namespace GoodInput
+{
+	int getInt();
+	float getFloat();
+	double getDouble();
+	char getChar();
+	void ignoreLine();
+}
+#endif
